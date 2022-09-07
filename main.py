0946ccc961c54efcfe89b37591f92aec50e4300e
Lee-lix <71934346+Lee-lix@users.noreply.github.com>
2022-09-07 10:43:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,13 @@ def get_birthday():
     next = next.replace(year=next.year + 1)
   return (next - today).days
 
+#男朋友的生日
+def get_birthday():
+  next = datetime.strptime(str(date.today().year) + "-" + birthday1, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  return (next - today).days
+
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
   if words.status_code != 200:
@@ -48,6 +55,6 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"birthday_left1":{"value":get_birthday1()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
